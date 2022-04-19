bce3ebc2228e08b746cb2dfe5cc99fc05f2fd073
William Robersone <wmrobersone@yahoo.com>
2022-04-18 20:39:51 -0500

    media queries

--- script.js
+++ script.js
@@ -2,111 +2,111 @@ let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.navbar');
 
 menu.onclick = () => {
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+};
 
 // for window scroll
 window.onscroll = () => {
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
 
-    if (window.scrollY > 0) {
-        document.querySelector('.header').classList.add('active');
-    } else {
-        document.querySelector('.header').classList.remove('active');
-    }
-}
+  if (window.scrollY > 0) {
+    document.querySelector('.header').classList.add('active');
+  } else {
+    document.querySelector('.header').classList.remove('active');
+  }
+};
 
 window.onload = () => {
-    if (window.scrollY > 0) {
+  if (window.scrollY > 0) {
     document.querySelector('.header').classList.add('active');
-    } else {
-        document.querySelector('.header').classList.remove('active');
-    }
-}
+  } else {
+    document.querySelector('.header').classList.remove('active');
+  }
+};
 
 // for home section
-var swiper = new Swiper(".home-slider", {
-    spaceBetween: 20,
-    effect: "fade",
-    grabCursor: true,
-    loop: true,
-    centeredSlides: true,
-    autoplay: {
-        delay: 9500,
-        disableOnInteraction: false,
-    },
+var swiper = new Swiper('.home-slider', {
+  spaceBetween: 20,
+  effect: 'fade',
+  grabCursor: true,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
 });
 
 // for feature section
-var swiper = new Swiper(".feature-slider", {
-    spaceBetween: 20,
-    grabCursor: true,
-    loop: true,
-    centeredSlides: true,
-    autoplay: {
-        delay: 9500,
-        disableOnInteraction: false,
+var swiper = new Swiper('.feature-slider', {
+  spaceBetween: 20,
+  grabCursor: true,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  // feature classes in columns
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-// feature classes in columns
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        768: {
-            slidesPerView: 2,
-        },
-        991: {
-            slidesPerView: 3,
-        },
+    768: {
+      slidesPerView: 2,
     },
+    991: {
+      slidesPerView: 3,
+    },
+  },
 });
 
 // for trainer section
-var swiper = new Swiper(".trainer-slider", {
-    spaceBetween: 20,
-    grabCursor: true,
-    loop: true,
-    centeredSlides: true,
-    autoplay: {
-        delay: 9500,
-        disableOnInteraction: false,
+var swiper = new Swiper('.trainer-slider', {
+  spaceBetween: 20,
+  grabCursor: true,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  // feature classes in columns
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-// feature classes in columns
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        768: {
-            slidesPerView: 2,
-        },
-        991: {
-            slidesPerView: 3,
-        },
+    768: {
+      slidesPerView: 2,
     },
+    991: {
+      slidesPerView: 3,
+    },
+  },
 });
 
 // blog section
-var swiper = new Swiper(".blogs-slider", {
-    spaceBetween: 20,
-    grabCursor: true,
-    loop: true,
-    centeredSlides: true,
-    autoplay: {
-        delay: 8500,
-        disableOnInteraction: false,
+var swiper = new Swiper('.blogs-slider', {
+  spaceBetween: 20,
+  grabCursor: true,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 8500,
+    disableOnInteraction: false,
+  },
+  // feature classes in columns
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-// feature classes in columns
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        768: {
-            slidesPerView: 2,
-        },
-        991: {
-            slidesPerView: 3,
-        },
+    768: {
+      slidesPerView: 2,
     },
+    991: {
+      slidesPerView: 3,
+    },
+  },
 });
